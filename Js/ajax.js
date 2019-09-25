3b56d3f21e841e0c0b1d35d0c4e0d0b99410e8c8
Joshua Coleman <procoleman77@outlook.com>
2019-09-24 22:39:51 -0400

    Ajax calls for dinning api

--- Js/ajax.js
+++ Js/ajax.js
@@ -1,65 +1,31 @@
-// Zomato Ajax calls
+// The mealdb Ajax calls
  console.log('test');
  console.log($);
 
-// $(document).ready(function () {
+//  
 
-    $("#getMessage").on("click", function () {
-        var valueSearchBox = $('#getText').val()
-        if (valueSearchBox === "") {
-            return;
-        }
-        select();
-    });
-    //--------------------------------------------------SEARCH BY CITY-----------------------------------------
 
-    function select() {
-        var valueDropdown = $('#select_id').val();
-        var valueSearchBox = $('#getText').val()
-        var searchCity = "&q=" + valueSearchBox;
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://developers.zomato.com/api/v2.1/search?entity_id=" + valueDropdown + "&entity_type=city" + searchCity + "&count=100",
-            "method": "GET",
-            "headers": {
-                "user-key": "ba1e1bdc5ea795af460a2abe393173a4",
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }
-        console.log(searchCity );
-        $.getJSON(settings, function (data) {
+// Ajax Calls for api
+var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Salmon";
 
-            data = data.restaurants;
-            var html = "";
 
-            $.each(data, function (index, value) {
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+    console.log("Object Data:",response);
+    console.log("Recipes Array:",response.meals);
+  console.log("Recipe Selected:",response.meals[0]);
+  console.log("Recipe:",response.meals[0].strMeal);
+  console.log("Recipe Instructions:",response.meals[0].strInstructions);
+  console.log("Recipe Link:",response.meals[0].strSource);
+  console.log("Recipe Picture:",response.meals[0].strMealThumb);
+  console.log("Cuisine:",response.meals[0].strArea);
+  console.log("Type:",response.meals[0].strCategory);
+  console.log("Type:",response.meals[0].strYoutube);
+  
 
-                var x = data[index];
-                $.each(x, function (index, value) {
-                    var location = x.restaurant.location;
-                    var userRating = x.restaurant.user_rating;
-                    // html += "<div class='data img-rounded'>";
-                    // html += "<div class='rating'>";
+  
 
-                    // html += "<span title='" + userRating.rating_text + "'><p style='color:white;background-color:#" + userRating.rating_color + ";border-radius:4px;border:none;padding:2px 10px 2px 10px;text-align: center;text-decoration:none;display:inline-block;font-size:16px;float:right;'><strong>" + userRating.aggregate_rating + "</strong></p></span><br>";
-                    // html += "  <strong class='text-info'>" + userRating.votes + " votes</strong>";
-                    // html += "</div>";
-                    // html += "<img class='resimg img-rounded' src=" + value.thumb + " alt='Restaurant Image' height='185' width='185'>";
-                    // html += "<a href=" + value.url + " target='_blank' class='action_link'><h2 style='color:red;'><strong>" + value.name + "</strong></h2></a>";
-                    // html += "  <strong class='text-primary'>" + location.locality + "</strong><br>";
-                    // html += "  <h6 style='color:grey;'><strong>" + location.address + "</strong></h6><hr>";
-                    // html += "  <strong>CUISINES</strong>: " + value.cuisines + "<br>";
-                    // html += "  <strong>COST FOR TWO</strong>: " + value.currency + value.average_cost_for_two + "<br>";
-                    // html += "</div><br>";
-                });
-            });
-            //   $(".message").html(html);
-        });
-
-    }
-    //--------------------------------------------------------------------------------------------------------
-    // $("#select_id").change(function() {
-    //  select();
-// });
-//    });
+  response.data
+});
