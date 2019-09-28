ca78a0c47b2d4550c6813ca3f0cc21a8e179de49
maiada <maiadacx@gmail.com>
2019-09-28 10:10:07 -0400

    got zomato working

--- assets/js/mealIDB-api.js
+++ assets/js/mealIDB-api.js
@@ -1,139 +1,193 @@
 // Initial Values
 // TBA
 
+var user = "homebody";
+var recipeQuery1;
+var recipeQuery2;
 
-// Ajax Calls for api mealbd
-var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Salmon";
+if (user === "athletic") {
+var recipeQuery1 = "https://www.themealdb.com/api/json/v1/1/search.php?s=Salad";
+var recipeQuery2 = "https://www.themealdb.com/api/json/v1/1/search.php?s=Lentils";
+} else if (user === "foodie") {
+  var recipeQuery1 = "https://www.themealdb.com/api/json/v1/1/search.php?s=French";
+  var recipeQuery2 = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese";
+} else if (user === "party") {
+  var recipeQuery1 = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Pasta";
+  var recipeQuery2 = "https://www.themealdb.com/api/json/v1/1/filter.php?a=American";
+} else if (user === "quirky") {
+  var recipeQuery1 = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian";
+  var recipeQuery2 = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan";
+} else if (user === "homebody") {
+  var recipeQuery1 = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast";
+  var recipeQuery2 = "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast";
+}
 
 
 $.ajax({
-  url: queryURL,
+  url: recipeQuery1,
   method: "GET"
-}).then(function(response) {
-  // Testing/Debug Ajax
-  //    console.log("Object Data:",response);
-  //    console.log("Recipes Array:",response.meals);
-  //  console.log("Recipe Selected:",response.meals[0]);
-  //  console.log("Recipe:",response.meals[0].strMeal);
-  //  console.log("Recipe Instructions:",response.meals[0].strInstructions);
-  //  console.log("Recipe Link:",response.meals[0].strSource);
-  //  console.log("Recipe Picture:",response.meals[0].strMealThumb);
-  //  console.log("Cuisine:",response.meals[0].strArea);
-  //  console.log("Type:",response.meals[0].strCategory);
-  // console.log("Type:",response.meals[0].strYoutube);
-  
+}).then(function (response) {
+
   // Empty the cards in html
 
-  $("#recipes-div").empty();
+  $("#recipes-div-1").empty();
 
-  for( var i=0 ; i < 3 ; i++){
-
-    console.log(response);
+  for (var i = 0; i < 3; i++) {
 
     // new  horizontal card
-   var newCard= $("<div>");
+    var newCard = $("<div>");
     newCard.addClass("card horizontal");
 
-  //  new card image
-  var cardImgDiv = $("<div>");
-  cardImgDiv.addClass("card-image");
-  var cardImg = $("<img>");
-  cardImg.attr("src", response.meals[i].strMealThumb);
-  cardImg.addClass("activator");
+    //  new card image
+    var cardImgDiv = $("<div>");
+    cardImgDiv.addClass("card-image");
+    var cardImg = $("<img>");
+    cardImg.attr("src", response.meals[i].strMealThumb);
+    cardImg.addClass("activator");
 
-  //card content
-  var cardStacked = $("<div>");
-  cardStacked.addClass("card-stacked");
+    //card content
+    var cardStacked = $("<div>");
+    cardStacked.addClass("card-stacked");
     //recipe title
-  var cardContent = $("<div>");
-  cardContent.html("<p>" + response.meals[i].strMeal + "</p>");
-  cardStacked.append(cardContent);
+    var cardContent = $("<div>");
+    cardContent.addClass("card-content");
+    var cardTitle = $("<p>");
+    cardTitle.text(response.meals[i].strMeal);
+    cardContent.append(cardTitle);
+    cardStacked.append(cardContent);
     //link
-  var cardLinkDiv = $("<div>");
-  cardLinkDiv.addClass("card-action");
-  cardLinkDiv.html("<a href= '" + response.meals[i].strSource + "' target='_blank'>" + "View Recipe" + "</a>")
+    var cardLinkDiv = $("<div>");
+    cardLinkDiv.addClass("card-action");
+    cardLinkDiv.html("<a href= '" + response.meals[i].strSource + "' target='_blank'>" + "View Recipe" + "</a>")
 
-// Puts everything on Page!
-$("#recipes-div").append(newCard);
-newCard.append(cardImgDiv);
-cardImgDiv.append(cardImg);
-newCard.append(cardStacked);
-cardStacked.append(cardLinkDiv);
-  
+    // Puts everything on Page!
+    $("#recipes-div-1").append(newCard);
+    newCard.append(cardImgDiv);
+    cardImgDiv.append(cardImg);
+    newCard.append(cardStacked);
+    cardStacked.append(cardLinkDiv);
   };
+
+  $.ajax({
+    url: recipeQuery2,
+    method: "GET"
+  }).then(function (response) {
+
+    // Empty the cards in html
+
+    $("#recipes-div-2").empty();
+
+    for (var i = 0; i < 3; i++) {
+
+      // new  horizontal card
+      var newCard = $("<div>");
+      newCard.addClass("card horizontal");
+
+      //  new card image
+      var cardImgDiv = $("<div>");
+      cardImgDiv.addClass("card-image");
+      var cardImg = $("<img>");
+      cardImg.attr("src", response.meals[i].strMealThumb);
+      cardImg.addClass("activator");
+
+      //card content
+      var cardStacked = $("<div>");
+      cardStacked.addClass("card-stacked");
+      //recipe title
+      var cardContent = $("<div>");
+      cardContent.addClass("card-content");
+      var cardTitle = $("<p>");
+      cardTitle.text(response.meals[i].strMeal);
+      cardContent.append(cardTitle);
+      cardStacked.append(cardContent);
+      //link
+      var cardLinkDiv = $("<div>");
+      cardLinkDiv.addClass("card-action");
+      cardLinkDiv.html("<a href= '" + response.meals[i].strSource + "' target='_blank'>" + "View Recipe" + "</a>")
+
+      // Puts everything on Page!
+      $("#recipes-div-2").append(newCard);
+      newCard.append(cardImgDiv);
+      cardImgDiv.append(cardImg);
+      newCard.append(cardStacked);
+      cardStacked.append(cardLinkDiv);
+
+    };
+
+  });
 
 });
 
 // FireBase Config
-var firebaseConfig = {
-  apiKey: "AIzaSyDi7sAhzFxcoGeoae7z8VI1jbT-UB_kAJA",
-  authDomain: "project-1-3f974.firebaseapp.com",
-  databaseURL: "https://project-1-3f974.firebaseio.com",
-  projectId: "project-1-3f974",
-  storageBucket: "project-1-3f974.appspot.com",
-  messagingSenderId: "1058372464183",
-  appId: "1:1058372464183:web:c9c2c7d3da3e564fc019b2",
-  measurementId: "G-H56C11S1CB"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// var firebaseConfig = {
+//   apiKey: "AIzaSyDi7sAhzFxcoGeoae7z8VI1jbT-UB_kAJA",
+//   authDomain: "project-1-3f974.firebaseapp.com",
+//   databaseURL: "https://project-1-3f974.firebaseio.com",
+//   projectId: "project-1-3f974",
+//   storageBucket: "project-1-3f974.appspot.com",
+//   messagingSenderId: "1058372464183",
+//   appId: "1:1058372464183:web:c9c2c7d3da3e564fc019b2",
+//   measurementId: "G-H56C11S1CB"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
 
-     // Create a variable to reference
-     var database = firebase.database();
-// Add click event/event listener 
-$("#add").on("click", function (event) {
-    
-  // Refresh
-  event.preventDefault();
-  // Grab values fromm text-boxes
-  train = $("").val().trim();
-  destination = $("#").val().trim();
-  frequency = $("#").val().trim();
-  nextArrive = $("#").val().trim();
+//      // Create a variable to reference
+//      var database = firebase.database();
+// // Add click event/event listener 
+// $("#add").on("click", function (event) {
 
-  // Test/Debug
-  // console.log(train);
-  // console.log(destination);
-  // console.log(frequency);
-  // console.log(nextArrive);
+//   // Refresh
+//   event.preventDefault();
+//   // Grab values fromm text-boxes
+//   train = $("").val().trim();
+//   destination = $("#").val().trim();
+//   frequency = $("#").val().trim();
+//   nextArrive = $("#").val().trim();
 
-  console.log(' jQuery value: ', $('#').val().trim());
-  console.log(' jQuery value: ', $('#').val().trim());
-  console.log(' jQuery value: ', $('#').val().trim());
+//   // Test/Debug
+//   // console.log(train);
+//   // console.log(destination);
+//   // console.log(frequency);
+//   // console.log(nextArrive);
 
-
-
-
-
-  // Code for "Setting values in the database"
-  database.ref().set({
-    train: train,
-    destination: destination,
-    frequency: frequency,
-    nextArrive: nextArrive
-  });
-// Add empty Event!!!
+//   console.log(' jQuery value: ', $('#').val().trim());
+//   console.log(' jQuery value: ', $('#').val().trim());
+//   console.log(' jQuery value: ', $('#').val().trim());
 
 
 
 
-});
 
-// Firebase watcher + initial loader HINT: .on("value")
-database.ref().on("value", function (snapshot) {
+//   // Code for "Setting values in the database"
+//   database.ref().set({
+//     train: train,
+//     destination: destination,
+//     frequency: frequency,
+//     nextArrive: nextArrive
+//   });
+// // Add empty Event!!!
 
-  // Log everything that's coming out of snapshot
-  console.log(snapshot.val());
-  console.log(snapshot.val().train);
-  console.log(snapshot.val().destination);
-  console.log(snapshot.val().frequency);
-  console.log(snapshot.val().nextArrive);
 
-  // Change the HTML to reflect
 
-  // Handle the errors
-}, function (errorObject) {
-  console.log("Errors handled: " + errorObject.code);
-});
+
+// });
+
+// // Firebase watcher + initial loader HINT: .on("value")
+// database.ref().on("value", function (snapshot) {
+
+//   // Log everything that's coming out of snapshot
+//   console.log(snapshot.val());
+//   console.log(snapshot.val().train);
+//   console.log(snapshot.val().destination);
+//   console.log(snapshot.val().frequency);
+//   console.log(snapshot.val().nextArrive);
+
+//   // Change the HTML to reflect
+
+//   // Handle the errors
+// }, function (errorObject) {
+//   console.log("Errors handled: " + errorObject.code);
+// });
