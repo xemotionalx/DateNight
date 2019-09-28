a8d34baab536af5ffba89587ae95b46b9531f180
maiada <maiadacx@gmail.com>
2019-09-28 11:34:56 -0400

    zomato and dining page are fully updated and functional

--- assets/js/zomato-api.js
+++ assets/js/zomato-api.js
@@ -1,31 +1,25 @@
 // Initial Values
 // TBA
 
-var user = "homebody";
-var restaurantQuery1;
-var restaurantQuery2;
+var user = "party";
+var restaurantQuery;
 
 if (user === "athletic") {
-var restaurantQuery1 = "https://www.themealdb.com/api/json/v1/1/search.php?s=Salad";
-var restaurantQuery2 = "https://www.themealdb.com/api/json/v1/1/search.php?s=Lentils";
+  restaurantQuery = "https://developers.zomato.com/api/v2.1/search?entity_id=287&entity_type=city&q=healthy&count=6";
 } else if (user === "foodie") {
-  var restaurantQuery1 = "https://www.themealdb.com/api/json/v1/1/search.php?s=French";
-  var restaurantQuery2 = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese";
+  restaurantQuery = "https://developers.zomato.com/api/v2.1/search?entity_id=287&entity_type=city&q=highend&count=6";
 } else if (user === "party") {
-  var restaurantQuery1 = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Pasta";
-  var restaurantQuery2 = "https://www.themealdb.com/api/json/v1/1/filter.php?a=American";
+  restaurantQuery = "https://developers.zomato.com/api/v2.1/search?entity_id=287&entity_type=city&q=nightlife&count=6";
 } else if (user === "quirky") {
-  var restaurantQuery1 = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian";
-  var restaurantQuery2 = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan";
+  restaurantQuery = "https://developers.zomato.com/api/v2.1/search?entity_id=287&entity_type=city&q=vegan&count=6";
 } else if (user === "homebody") {
-  var restaurantQuery1 = "https://developers.zomato.com/api/v2.1/search?entity_id=delivery&entity_type=cityphiladelphia&count=3";
-  var restaurantQuery2 = "https://developers.zomato.com/api/v2.1/search?entity_id=takeout&entity_type=cityphiladelphia&count=3";
+  restaurantQuery = "https://developers.zomato.com/api/v2.1/search?entity_id=287&entity_type=city&q=delivery&count=6";
 }
 
 //ba1e1bdc5ea795af460a2abe393173a4
 
 $.ajax({
-  url: restaurantQuery1,
+  url: restaurantQuery,
   method: "GET",
   "headers": {
     "user-key": "d710754ce67200fb6fb9b5e26139f50e",
@@ -33,103 +27,130 @@ $.ajax({
   }
 }).then(function (response) {
 
-
-  console.log("url: " + response.restaurants[0].restaurant.url);
-  console.log("photo: " + response.restaurants[0].restaurant.photos_url);
-  console.log("cuisine: " + response.restaurants[0].restaurant.cuisines)
-  console.log("average cost: $" + response.restaurants[0].restaurant.average_cost_for_two)
-  console.log(response.restaurants[0]);
-
-    console.log(response.restaurants[0]);
-
+  console.log(response);
 
   //Empty the cards in html
+  $("#restaurants-div").empty();
 
-  $("#restaurants-div-1").empty();
-
-  for (var i = 0; i < 3; i++) {
-    // new  horizontal card
-    var newCard = $("<div>");
-    newCard.addClass("card horizontal");
-
-    //  new card image
+  for (var i = 0; i < 6; i++) {
+    // create new card
+    var cardWidth = $("<div>");
+    cardWidth.addClass("col s12 m4");
+    var card = $("<div>");
+    card.addClass("card small");
     var cardImgDiv = $("<div>");
-    cardImgDiv.addClass("card-image");
+    cardImgDiv.addClass("card-image waves-effect waves-block waves-light");
+    //create card image
     var cardImg = $("<img>");
-    cardImg.attr("src", response.restaurants[i].restaurant.photos_url);
     cardImg.addClass("activator");
-
-    //card content
-    var cardStacked = $("<div>");
-    cardStacked.addClass("card-stacked");
-    //recipe title
+    if (response.restaurants[i].restaurant.photo_count === 0) {
+      cardImg.attr("src", "assets/images/dining/restaurant-icon.png");
+    } else {
+      cardImg.attr("src", response.restaurants[i].restaurant.photos[0].photo.url);
+    };
+    //card title
     var cardContent = $("<div>");
     cardContent.addClass("card-content");
-    var cardTitle = $("<p>");
+    var cardTitle = $("<span>");
+    cardTitle.addClass("card-title activator grey-text text-darken-4 truncate");
     cardTitle.text(response.restaurants[i].restaurant.name);
-    cardContent.append(cardTitle);
-    cardStacked.append(cardContent);
-    //link
-    var cardLinkDiv = $("<div>");
-    cardLinkDiv.addClass("card-action");
-    cardLinkDiv.html("<a href= '" + response.restaurants[i].restaurant.url + "' target='_blank'>" + "Learn More" + "</a>")
+    var cardIcon = $("<i>");
+    cardIcon.addClass("material-icons right");
+    cardIcon.text("more_vert");
+    //card link
+    var cardLink = $("<p>");
+    cardLink.html("<a href= '" + response.restaurants[i].restaurant.url + "' target='_blank'>" + "More Info" + "</a>");
 
-    // Puts everything on Page!
-    $("#restaurants-div-1").append(newCard);
-    newCard.append(cardImgDiv);
+    // append all front of card to div
+    $("#restaurants-div").append(cardWidth);
+    cardWidth.append(card);
+    card.append(cardImgDiv);
     cardImgDiv.append(cardImg);
-    newCard.append(cardStacked);
-    cardStacked.append(cardLinkDiv);
+    card.append(cardContent);
+  cardContent.append(cardTitle);
+  cardTitle.append(cardIcon);
+    cardContent.append(cardLink);
+
+    //back of card
+    var cardReveal = $("<div>");
+    cardReveal.addClass("card-reveal");
+
+    //back of card title
+    var cardRevealTitle = $("<span>");
+    cardRevealTitle.addClass("card-title grey-text text-darken-4");
+    cardRevealTitle.text("Restaurant Info");
+
+    //exit card reveal icon
+    var closeIcon = $("<i>");
+    closeIcon.addClass("material-icons right");
+    closeIcon.text("close");
+
+    //info on card reveal
+    var cardRevealContent = $("<div>");
+    cardRevealContent.addClass("card-content");
+    var cardBudget = $("<h6>");
+    cardBudget.text("Average Cost: $" + response.restaurants[0].restaurant.average_cost_for_two + " for two");
+    var cardCuisine = $("<h6>");
+    cardCuisine.text("Cuisine: " + response.restaurants[0].restaurant.cuisines);
+
+    card.append(cardReveal);
+    cardReveal.append(cardRevealTitle);
+    cardRevealTitle.append(closeIcon);
+    cardReveal.append(cardRevealContent);
+    cardRevealContent.append(cardBudget);
+    cardRevealContent.append(cardCuisine);
+
+
   };
 
-//   $.ajax({
-//     url: recipeQuery2,
-//     method: "GET"
-//   }).then(function (response) {
 
-//     // Empty the cards in html
+});
 
-//     $("#recipes-div-2").empty();
+  // new  horizontal card
+  //   var newCard = $("<div>");
+  //   newCard.addClass("card horizontal");
 
-//     for (var i = 0; i < 3; i++) {
+  //   //  new card image
+  //     var cardImgDiv = $("<div>");
+  //     cardImgDiv.addClass("card-image");
+  //     var cardImg = $("<img>");
+  //     cardImg.addClass("activator");
 
-//       console.log(response);
+  //     if (response.restaurants[i].restaurant.photo_count === 0) {
+  //       cardImg.attr("src", "assets/images/dining/restaurant-icon.png");
+  //     } else {
+  //       cardImg.attr("src", response.restaurants[i].restaurant.photos[0].photo.url);
+  //     }
 
-//       // new  horizontal card
-//       var newCard = $("<div>");
-//       newCard.addClass("card horizontal");
 
-//       //  new card image
-//       var cardImgDiv = $("<div>");
-//       cardImgDiv.addClass("card-image");
-//       var cardImg = $("<img>");
-//       cardImg.attr("src", response.meals[i].strMealThumb);
-//       cardImg.addClass("activator");
+  //   //card content
+  //   var cardStacked = $("<div>");
+  //   cardStacked.addClass("card-stacked");
 
-//       //card content
-//       var cardStacked = $("<div>");
-//       cardStacked.addClass("card-stacked");
-//       //recipe title
-//       var cardContent = $("<div>");
-//       cardContent.addClass("card-content");
-//       var cardTitle = $("<p>");
-//       cardTitle.text(response.meals[i].strMeal);
-//       cardContent.append(cardTitle);
-//       cardStacked.append(cardContent);
-//       //link
-//       var cardLinkDiv = $("<div>");
-//       cardLinkDiv.addClass("card-action");
-//       cardLinkDiv.html("<a href= '" + response.meals[i].strSource + "' target='_blank'>" + "View Recipe" + "</a>")
+  //   //recipe title & info
+  //   var cardContent = $("<div>");
+  //   cardContent.addClass("card-content");  
+  //   var cardTitle = $("<p>");
+  //   cardTitle.text(response.restaurants[i].restaurant.name);
+  //   var cardBudget = $("<h6>");
+  //   cardBudget.text("Average Cost: $" + response.restaurants[0].restaurant.average_cost_for_two + " for two");
+  //   var cardCuisine = $("<h6>");
+  //   cardCuisine.text("Cuisine: " + response.restaurants[0].restaurant.cuisines);
 
-//       // Puts everything on Page!
-//       $("#recipes-div-2").append(newCard);
-//       newCard.append(cardImgDiv);
-//       cardImgDiv.append(cardImg);
-//       newCard.append(cardStacked);
-//       cardStacked.append(cardLinkDiv);
+  //   //append title& info
+  //   cardContent.append(cardTitle);
+  //   cardContent.append(cardBudget);
+  //   cardContent.append(cardCuisine);
+  //   cardStacked.append(cardContent);
 
-//     };
+  //   //link
+  //   var cardLinkDiv = $("<div>");
+  //   cardLinkDiv.addClass("card-action");
+  //   cardLinkDiv.html("<a href= '" + response.restaurants[i].restaurant.url + "' target='_blank'>" + "Learn More" + "</a>")
 
-//   });
-
- });
+  //   // Puts everything on Page!
+  //   $("#restaurants-div").append(newCard);
+  //  newCard.append(cardImgDiv);
+  //  cardImgDiv.append(cardImg);
+  //   newCard.append(cardStacked);
+  //   cardStacked.append(cardLinkDiv);
